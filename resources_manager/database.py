1f7b4069be95d392aeac99a4759699795e307834
devKRT <devKRT@d.c>
2021-04-07 21:07:50 +0300

    The database ('data.db') and the module which work with it ('database.py') was update.

--- resources_manager/database.py
+++ resources_manager/database.py
@@ -8,7 +8,7 @@ def __sql(query: str, _path='data.db'):
     This function works with SQL-database (DB), does any queries (from SELECT to INSERT).
 
     :param query: SQL-query
-    :param path: path to the DB
+    :param _path: path to the DB
     :return result, that DB return on query 'query'
     """
     _path = path(_path)
@@ -31,6 +31,37 @@ def is_story_saved(story_id: int) -> bool:
     return bool(result)
 
 
-def write_story(story_id: int, user: str):
-    __sql(f"INSERT INTO stories(id, user) "
-          f"VALUES({story_id}, '{user}')")
+def write_story(story_id: int, user: str, current_datetime: str):
+    __sql(f"INSERT INTO stories(id, user, datetime) "
+          f"VALUES({story_id}, '{user}', '{current_datetime}')")
+
+
+def last_story_id(user: str):
+    return __sql(f"SELECT id "
+                 f"FROM stories "
+                 f"WHERE user = {user} "
+                 f"ORDER BY datetime DESC"
+                 f"LIMIT 1 ")
+
+
+def user_exist(user_id: int):
+    return bool(__sql(f"SELECT * "
+                      f"FROM users "
+                      f"WHERE id = {user_id}"))
+
+
+def add_user(user_id: int):
+    if not user_exist(user_id):
+        __sql(f"INSERT INTO users(id) "
+              f"VALUES({user_id}) ")
+
+
+def get_status(user_id):
+    if user_exist(user_id):
+        return __sql(f"SELECT status")
+
+
+def change_status(user_id: str, status: bool):
+    __sql(f"UPDATE users "
+          f"SET status = '{status}' "
+          f"WHERE id = {user_id}")
