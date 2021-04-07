96538e5dca2f839f3c6f2158f2c52b3933663689
devKRT <devKRT@d.c>
2021-04-07 21:01:28 +0300

    The starting module 'main.py' was added.

--- main/main.py
+++ main/main.py
@@ -1,11 +1,13 @@
 from asyncio import new_event_loop
 
 from insta.bot import main as insta_bot
+from teleg.bot import main as telegram_bot
 
 
 def main():
     loop = new_event_loop()
     loop.create_task(insta_bot())
+    loop.create_task(telegram_bot())
     loop.run_forever()
 
 
